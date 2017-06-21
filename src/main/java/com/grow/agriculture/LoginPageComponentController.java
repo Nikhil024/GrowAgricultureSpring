8bc510cfc8477c61cbf6309ca8ee4ad3c7145865
nikhil mohan <nikhil mohan>
2017-06-21 08:01:55 +0530

    2.0.1

--- src/main/java/com/grow/agriculture/LoginPageComponentController.java
+++ src/main/java/com/grow/agriculture/LoginPageComponentController.java
@@ -4,30 +4,32 @@ import org.apache.commons.configuration.ConfigurationException;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.grow.agriculture.service.ConfigurationService;
-import com.grow.agriculture.service.JsonReaderService;
 
 @Controller
 public class LoginPageComponentController {
-	
+
 	private static final Logger LOG = Logger.getLogger(LoginPageComponentController.class);
-	
-	@Autowired
-	JsonReaderService jsonReadService;
-	
+
+	/*@Autowired
+	JsonReaderService jsonReadService;*/
+
 	@Autowired
 	ConfigurationService configurationService;
-	
+
 	private static final String VIEW_NAME = "loginPageComponent";
 
 	@RequestMapping(value="/login", method = RequestMethod.GET)
-	public String loginGetComponent() throws ConfigurationException{
-		jsonReadService.demo();
-		LOG.info(configurationService.getConfiguration().getInt("demo"));
+	public String loginGetComponent(Model model) throws ConfigurationException{
+		model.addAttribute("","");
+		
+		
+		
 		return VIEW_NAME;
 	}
-	
+
 }

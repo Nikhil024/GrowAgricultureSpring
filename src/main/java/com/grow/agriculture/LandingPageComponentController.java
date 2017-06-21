aab29e9aab69d4a3c81e7fde79c9ec1669af0daa
Nikhil <nikhil.iy@gmail.com>
2017-06-21 18:25:34 +0530

    2.0.1

--- src/main/java/com/grow/agriculture/LandingPageComponentController.java
+++ src/main/java/com/grow/agriculture/LandingPageComponentController.java
@@ -1,23 +1,45 @@
 package com.grow.agriculture;
 
 
+import javax.security.auth.login.Configuration;
+
+import org.apache.commons.configuration.ConfigurationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.grow.agriculture.additional.GrowAgricultureConstants;
+import com.grow.agriculture.service.ConfigurationService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class LandingPageComponentController {
+	
+	@Autowired
+	ConfigurationService configurationService;
+	
 	public static final String VIEW_NAME =  "landingPageComponent";
+	public static final String PROJECT_NAME = "project_name";
+	public static final String LANDING_PAGE_LOGO_IMAGE_NAME = "landing_page_logo_image_name";
+	public static final String LANDING_PAGE_BACKGROUND_IMAGE_NAME = "landing_page_background_image_name";
+	public static final String LANDING_PAGE_DESCRIPTION = "landing_page_description";
+	
 	
 	private static final Logger logger = LoggerFactory.getLogger(LandingPageComponentController.class);
 	
 	@RequestMapping(value="/", method = RequestMethod.GET)
-	public String landingPageMethod(){
+	public String landingPageMethod(Model model) throws ConfigurationException{
+		
+		model.addAttribute(PROJECT_NAME,configurationService.getConfiguration().getString(GrowAgricultureConstants.PROJECT_NAME));
+		model.addAttribute(LANDING_PAGE_LOGO_IMAGE_NAME,configurationService.getConfiguration().getString(GrowAgricultureConstants.LANDING_PAGE_LOGO_IMAGE_NAME));
+		model.addAttribute(LANDING_PAGE_BACKGROUND_IMAGE_NAME,configurationService.getConfiguration().getString(GrowAgricultureConstants.LANDING_PAGE_BACKGROUND_IMAGE_NAME));
+		model.addAttribute(LANDING_PAGE_DESCRIPTION,configurationService.getConfiguration().getString(GrowAgricultureConstants.LANDING_PAGE_DESCRIPTION));
 		return VIEW_NAME;
 	}
 }
--- src/main/java/com/grow/agriculture/additional/GrowAgricultureConstants.java
+++ src/main/java/com/grow/agriculture/additional/GrowAgricultureConstants.java
@@ -2,6 +2,19 @@ package com.grow.agriculture.additional;
 
 public class GrowAgricultureConstants {
 	
-	public static final String JSON_MESSAGE_URL = ""; 
-
+	public static final String PROJECT_NAME = "growAgriculture.project.name";
+	public static final String LANDING_PAGE_LOGO_IMAGE_NAME = "growAgriculture.landingMainPage.logo.image.name";
+	public static final String LANDING_PAGE_DESCRIPTION = "growAgriculture.landingMainPage.description";
+	public static final String LANDING_PAGE_BACKGROUND_IMAGE_NAME = "growAgriculture.landingMainPage.background.image.name";
+	public static final String LOGIN_PAGE_BACKGROUND_IMAGE_NAME = "growAgriculture.Login.background.image.name";
+	public static final String REGISTER_PAGE_BACKGROUND_IMAGE_NAME= "growAgriculture.Register.background.image.name";
+	public static final String FORGOTPASSWORD_PAGE_BACKGROUND_IMAGE_NAME = "growAgriculture.ForgotPassword.background.image.name";
+	public static final String ABOUTUS_PAGE_BACKGROUND_IMAGE_NAME = "growAgriculture.aboutUs.background.image.name";
+	public static final String LOGIN_TITLE_NAME = "growAgriculture.login.title.name";
+	public static final String ABOUTUS_TITLE_NAME = "growAgriculture.aboutUs.title.name";
+	public static final String FORGOTPASSWORD_TITLE_NAME = "growAgriculture.forgotPassword.title.name";
+	public static final String REGISTER_TITLE_NAME = "growAgriculture.Register.title.name";
+	public static final String BUYERLOGIN_TITLE_NAME = "growAgriculture.buyerLogin.title.name";
+	public static final String FARMERLOGIN_TITILE_NAME = "growAgriculture.farmerLogin.title.name";
+	public static final String LOGINANDREGISTER_INPUT_PLACEHOLDER_TEXT = "growAgriculture.loginAndRegister.input.placeholder.text";
 }
